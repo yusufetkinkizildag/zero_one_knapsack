151fca6c37c1b848a29eca00f41a06256df59dc6
Yusuf Etkin KIZILDAĞ <49615906+yusufetkinkizildag@users.noreply.github.com>
2022-10-26 23:07:23 +0300

    Update zero_one_knapscak.cpp

--- src/zero_one_knapscak.cpp
+++ src/zero_one_knapscak.cpp
@@ -344,3 +344,78 @@ int main(int argc, char *argv[]) {
   maxProfit = ks.solveKnapsack(profits, weights, 6);
   cout << "Total knapsack profit ---> " << maxProfit << endl;
 }
+//****************************************************************** benim eski cozum
+#include <iostream>
+
+#define NUMBER_OF_ITEMS 4
+#define KNAPSACK_CAPACITY 5
+
+void print_array(int *array, int array_length)
+{
+    for (int i = 0; i < array_length; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int knapsack_recursive(int weights[], int values[], int number_of_items, int knapsack_capacity)
+{
+    if (knapsack_capacity == 0) //if the capacity of knapsack is 0 then can not include any item
+    {
+        return 0;
+    }
+
+    if (number_of_items == 0) //if no more items are left then return
+    {
+        return 0;
+    }
+
+    if (weights[number_of_items - 1] > knapsack_capacity) //if current item weighs more than the capacity of knapsack, it can not be included
+    {
+        return knapsack_recursive(weights, values, number_of_items - 1, knapsack_capacity);
+    }
+
+    //else select the maximum value of once including the current item and once not including it
+    return std::max(knapsack_recursive(weights, values, number_of_items - 1, knapsack_capacity), values[number_of_items - 1] + knapsack_recursive(weights, values, number_of_items - 1, knapsack_capacity - weights[number_of_items - 1]));
+}
+
+int knapsack_dynamic(int weights[], int values[], int number_of_items, int knapsack_capacity)
+{
+    int i, j;
+    int knapsack[number_of_items + 1][knapsack_capacity + 1];
+    for (i = 0; i <= number_of_items; i++)
+    {
+        knapsack[i][0] = 0;
+    }
+    for (j = 0; j <= knapsack_capacity; j++)
+    {
+        knapsack[0][j] = 0;
+    }
+
+    for (i = 1; i <= number_of_items; i++)
+    {
+        for (j = 1; j <= knapsack_capacity; j++)
+        {
+            if (weights[i - 1] <= j)
+            {
+                knapsack[i][j] = std::max(knapsack[i - 1][j], values[i - 1] + knapsack[i - 1][j - weights[i - 1]]);
+            }
+            else
+            {
+                knapsack[i][j] = knapsack[i - 1][j];
+            }
+        }
+    }
+    
+    return knapsack[number_of_items][knapsack_capacity];
+}
+
+int main(int argc, char const *argv[])
+{
+    int weights[NUMBER_OF_ITEMS] = {2, 1, 3, 2};    // item one is 2kg, item two is 1kg, item three is 3kg, item four is 2kg
+    int values[NUMBER_OF_ITEMS] = {12, 10, 20, 15}; // item one is 12$, item two is 10$, item three is 20$, item four is 15$
+    std::cout << knapsack_recursive(weights, values, NUMBER_OF_ITEMS, KNAPSACK_CAPACITY) << "$\n";
+    std::cout << knapsack_dynamic(weights, values, NUMBER_OF_ITEMS, KNAPSACK_CAPACITY) << "$\n";
+    return 0;
+}
