5c0cfd0665cbb8fec044f23740dff03b447de47e
Yusuf Etkin KIZILDAĞ <etkin866@gmail.com>
2022-11-04 23:44:26 +0300

    dynamic

--- src/zero_one_knapsack.cpp
+++ src/zero_one_knapsack.cpp
@@ -3,6 +3,7 @@
 
 namespace recursive
 {
+
     constexpr static auto solve_recursive{[](auto const &profits, auto const &weights, auto const capacity, auto const current_index) noexcept
     {
         if (capacity <= 0 || current_index >= profits.size())
@@ -22,10 +23,12 @@ namespace recursive
     {
         return solve_recursive(profits, weights, capacity, 0);
     }};
+
 } // namespace recursive
 
 namespace memoization
 {
+
     constexpr static auto solve_recursive{[](auto &dp, auto const &profits, auto const &weights, auto const capacity, auto const current_index) noexcept
     {
         if (capacity <= 0 || current_index >= profits.size())
@@ -51,7 +54,43 @@ namespace memoization
         std::vector<std::vector<int>> dp(profits.size(), std::vector<int>(capacity + 1, -1));
         return solve_recursive(dp, profits, weights, capacity, 0);
     }};
+
 } // namespace memoization
+
+namespace dynamic
+{
+    constexpr static auto solve{[](auto const &profits, auto const &weights, auto const capacity) noexcept
+    {
+        if (capacity <= 0 || profits.empty() || weights.size() != profits.size())
+        {
+            return 0;
+        }
+        auto const n{profits.size()};
+        std::vector<std::vector<int>> dp(n, std::vector<int>(capacity + 1, 0));
+        for (int c = 0; c <= capacity; c++)
+        {
+            if (weights[0] <= c)
+            {
+                dp[0][c] = profits[0];
+            }
+        }
+        for (auto i{1}; i < n; ++i)
+        {
+            for (auto c{1}; c <= capacity; ++c)
+            {
+                auto profit1{0};
+                auto profit2{0};
+                if (weights[i] <= c)
+                {
+                    profit1 = profits[i] + dp[i - 1][c - weights[i]];
+                }
+                profit2 = dp[i - 1][c];
+                dp[i][c] = std::max(profit1, profit2);
+            }
+        }
+        return dp[n - 1][capacity];
+    }};
+} // namespace dynamic
 
 int main(int argc, char const *argv[])
 {
@@ -63,67 +102,12 @@ int main(int argc, char const *argv[])
     std::cout << "recursive with memoization" << std::endl;
     std::cout << memoization::solve(profits, weights, 6) << std::endl;
     std::cout << memoization::solve(profits, weights, 7) << std::endl;
+    std::cout << "dynamic" << std::endl;
+    std::cout << dynamic::solve(profits, weights, 6) << std::endl;
+    std::cout << dynamic::solve(profits, weights, 7) << std::endl;
     return 0;
 }
 
-
-// using namespace std;
-
-// #include <iostream>
-// #include <vector>
-
-// class Knapsack {
-// public:
-//   int solveKnapsack(const vector<int> &profits, const vector<int> &weights, int capacity) {
-//     // basic checks
-//     if (capacity <= 0 || profits.empty() || weights.size() != profits.size()) {
-//       return 0;
-//     }
-
-//     int n = profits.size();
-//     vector<vector<int>> dp(n, vector<int>(capacity + 1));
-
-//     // populate the capacity=0 columns, with '0' capacity we have '0' profit
-//     for (int i = 0; i < n; i++) {
-//       dp[i][0] = 0;
-//     }
-
-//     // if we have only one weight, we will take it if it is not more than the capacity
-//     for (int c = 0; c <= capacity; c++) {
-//       if (weights[0] <= c) {
-//         dp[0][c] = profits[0];
-//       }
-//     }
-
-//     // process all sub-arrays for all the capacities
-//     for (int i = 1; i < n; i++) {
-//       for (int c = 1; c <= capacity; c++) {
-//         int profit1 = 0, profit2 = 0;
-//         // include the item, if it is not more than the capacity
-//         if (weights[i] <= c) {
-//           profit1 = profits[i] + dp[i - 1][c - weights[i]];
-//         }
-//         // exclude the item
-//         profit2 = dp[i - 1][c];
-//         // take maximum
-//         dp[i][c] = max(profit1, profit2);
-//       }
-//     }
-
-//     // maximum profit will be at the bottom-right corner.
-//     return dp[n - 1][capacity];
-//   }
-// };
-
-// int main(int argc, char *argv[]) {
-//   Knapsack ks;
-//   vector<int> profits = {1, 6, 10, 16};
-//   vector<int> weights = {1, 2, 3, 5};
-//   int maxProfit = ks.solveKnapsack(profits, weights, 6);
-//   cout << "Total knapsack profit ---> " << maxProfit << endl;
-//   maxProfit = ks.solveKnapsack(profits, weights, 7);
-//   cout << "Total knapsack profit ---> " << maxProfit << endl;
-// }
 
 // // print elements
 
