aecd83d76aeb71a8cc0dbb8ef466023fa7ccad11
Yusuf Etkin KIZILDAĞ <etkin866@gmail.com>
2022-11-05 00:02:19 +0300

    update

--- src/zero_one_knapsack.cpp
+++ src/zero_one_knapsack.cpp
@@ -60,6 +60,27 @@ namespace memoization
 namespace dynamic
 {
 
+    constexpr static auto print_selected_element{[](auto const &dp, auto const &weights, auto const &profits, auto const capacity) noexcept
+    {
+        auto const n{weights.size() - 1};
+        auto cap{capacity};
+        auto total_profit{dp[n][capacity]};
+        for(auto i{n}; i > 0; i--)
+        {
+            if (total_profit != dp[i - 1][cap])
+            {
+                std::cout << " " << weights[i];
+                cap -= weights[i];
+                total_profit -= profits[i];
+            }
+            if (total_profit != 0)
+            {
+                std::cout << " " << weights[0];
+            }
+        }
+        std::cout << '\n';
+    }};
+
     constexpr static auto solve{[](auto const &profits, auto const &weights, auto const capacity) noexcept
     {
         if (capacity <= 0 || profits.empty() || weights.size() != profits.size())
@@ -89,6 +110,7 @@ namespace dynamic
                 dp[i][c] = std::max(profit1, profit2);
             }
         }
+        print_selected_element(dp, weights, profits, capacity);
         return dp[n - 1][capacity];
     }};
 
@@ -109,87 +131,6 @@ int main(int argc, char const *argv[])
     std::cout << dynamic::solve(profits, weights, 7) << std::endl;
     return 0;
 }
-
-
-// // print elements
-
-// using namespace std;
-
-// #include <iostream>
-// #include <vector>
-
-// class Knapsack {
-// public:
-//   int solveKnapsack(const vector<int> &profits, const vector<int> &weights, int capacity) {
-//     // base checks
-//     if (capacity <= 0 || profits.empty() || weights.size() != profits.size()) {
-//       return 0;
-//     }
-
-//     int n = profits.size();
-//     vector<vector<int>> dp(n, vector<int>(capacity + 1));
-
-//     // populate the capacity=0 columns, with '0' capacity we have '0' profit
-//     for (int i = 0; i < n; i++) {
-//       dp[i][0] = 0;
-//     }
-
-//     // if we have only one weight, we will take it if it is not more than the capacity
-//     for (int c = 0; c <= capacity; c++) {
-//       if (weights[0] <= c) {
-//         dp[0][c] = profits[0];
-//       }
-//     }
-
-//     // process all sub-arrays for all the capacities
-//     for (int i = 1; i < n; i++) {
-//       for (int c = 1; c <= capacity; c++) {
-//         int profit1 = 0, profit2 = 0;
-//         // include the item, if it is not more than the capacity
-//         if (weights[i] <= c) {
-//           profit1 = profits[i] + dp[i - 1][c - weights[i]];
-//         }
-//         // exclude the item
-//         profit2 = dp[i - 1][c];
-//         // take maximum
-//         dp[i][c] = max(profit1, profit2);
-//       }
-//     }
-
-//     printSelectedElements(dp, weights, profits, capacity);
-//     // maximum profit will be at the bottom-right corner.
-//     return dp[n - 1][capacity];
-//   }
-
-// private:
-//   void printSelectedElements(vector<vector<int>> &dp, const vector<int> &weights,
-//                              const vector<int> &profits, int capacity) {
-//     cout << "Selected weights:";
-//     int totalProfit = dp[weights.size() - 1][capacity];
-//     for (int i = weights.size() - 1; i > 0; i--) {
-//       if (totalProfit != dp[i - 1][capacity]) {
-//         cout << " " << weights[i];
-//         capacity -= weights[i];
-//         totalProfit -= profits[i];
-//       }
-//     }
-
-//     if (totalProfit != 0) {
-//       cout << " " << weights[0];
-//     }
-//     cout << "" << endl;
-//   }
-// };
-
-// int main(int argc, char *argv[]) {
-//   Knapsack ks;
-//   vector<int> profits = {1, 6, 10, 16};
-//   vector<int> weights = {1, 2, 3, 5};
-//   int maxProfit = ks.solveKnapsack(profits, weights, 7);
-//   cout << "Total knapsack profit ---> " << maxProfit << endl;
-//   maxProfit = ks.solveKnapsack(profits, weights, 6);
-//   cout << "Total knapsack profit ---> " << maxProfit << endl;
-// }
 
 // using namespace std;
 
